10d7e13caa56567ba7cda641ea5222386aedfbf8
shifas <shifasshamsu2003@gmail.com>
2026-03-02 00:17:59 +0530

    Configure ALLOWED_HOSTS and CSRF for Railway

--- MMM_AuditFlow/settings.py
+++ MMM_AuditFlow/settings.py
@@ -30,11 +30,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [".railway.app"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://mmm-auditing-company-production.up.railway.app',
-
+    "https://*.railway.app",
 ]
 
 
